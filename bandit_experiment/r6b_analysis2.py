55833721c08d17425331f2eb842e4739d7a9c197
xbb <xbb66kw@gmail.com>
2017-04-27 20:00:49 +0800

    Update r6b_analysis2.py

--- bandit_experiment/r6b_analysis2.py
+++ bandit_experiment/r6b_analysis2.py
@@ -7,49 +7,49 @@ import gzip
 import re
 
 class Test(object):
-				def __init__(self):
-								print('test1')
+    def __init__(self):
+        print('test1')
 
 
 def main():
-				k = 50
-				K = 5
-				f = open("/Users/xbb/Desktop/bandit_experiment/extracted_data_568610.txt")
-				'/Users/xbb/Desktop/bandit_experiment/extracted_data_568610.txt'
-				
-				cha = f.readline()
-				nodes = {}
-				i = 0
-				line_number = 0
-				while cha:
-				
-								
-								result = np.array([int(i) for i in list(cha) if i != '\n'])
-								nodes[i] = result
-								i += 1
-								cha = f.readline()
-								line_number += 1
-				
-				
-				select = np.random.choice(line_number, k, replace = False)
-				total = 0
-				for index, key in enumerate(nodes):
-								if any_connection_in_k(select, nodes[key], nodes, K):
-												total += 1
-				
-				print(total)
-				
+    k = 50
+    K = 5
+    f = open("/Users/xbb/Desktop/bandit_experiment/extracted_data_568610.txt")
+    '/Users/xbb/Desktop/bandit_experiment/extracted_data_568610.txt'
+    
+    cha = f.readline()
+    nodes = {}
+    i = 0
+    line_number = 0
+    while cha:
+    
+        
+        result = np.array([int(i) for i in list(cha) if i != '\n'])
+        nodes[i] = result
+        i += 1
+        cha = f.readline()
+        line_number += 1
+    
+    
+    select = np.random.choice(line_number, k, replace = False)
+    total = 0
+    for index, key in enumerate(nodes):
+        if any_connection_in_k(select, nodes[key], nodes, K):
+            total += 1
+    
+    print(total)
+    
 def distant(A, B):
-				return np.sum(np.power((A - B), 2))
+    return np.sum(np.power((A - B), 2))
 
 
 def any_connection_in_k(select, node, nodes, K):
-				'node: array-like'
-				for key in select:
-								if distant(node, nodes[key]) <= K:
-												return True
-												
-				return False
+    'node: array-like'
+    for key in select:
+        if distant(node, nodes[key]) <= K:
+            return True
+            
+    return False
 if __name__ == '__main__':
     main()
     
